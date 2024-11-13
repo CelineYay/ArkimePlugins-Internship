1f0d8cbd21c264bca698e177c48f7b737b3d1ccf
CelineChan <celinechan@localhost.localdomain>
2024-11-13 14:20:39 +0800

    env rm

--- testFunction.lua
+++ testFunction.lua
@@ -1,5 +1,12 @@
 -- function_testluaunit.lua
 -- /home/celinechan/Desktop/Repo/ArkimePlugins-Internship/function_testluaunit.lua
+local version = _VERSION:match("%d+%.%d+")
+
+package.path = "lua_modules/share/lua/" .. version ..
+    "/?.lua;lua_modules/share/lua/" .. version .. "/?/init.lua;" ..
+    package.path .. ";src/?.lua"
+package.cpath = "lua_modules/lib/lua/" .. version .. "/?.so;" .. package.cpath
+
 lu = require "luaunit"
 math_functions = require("math_functions")
 
